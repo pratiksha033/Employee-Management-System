497cf880552e27c1745ab336722849ea4cf12331
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-09-15 17:25:55 +0530

    route for get departement (root route) added

--- backend/routes/department.js
+++ backend/routes/department.js
@@ -1,9 +1,10 @@
 import express from "express";
 // import {isAuthenticated} from "../middleware/authMiddleware.js";
-import { addDepartment } from "../controllers/departmentController.js";
+import { addDepartment, getDepartments } from "../controllers/departmentController.js";
 
 
 const router = express.Router();
+router.get("/",getDepartments)
 router.post("/add",addDepartment)
 
 export default router
