ecb4e9b4092162d90a23771d229bc1912aecc0ba
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-09-16 16:03:32 +0530

    edit and delte routes created

--- backend/routes/department.js
+++ backend/routes/department.js
@@ -1,10 +1,15 @@
 import express from "express";
 // import {isAuthenticated} from "../middleware/authMiddleware.js";
-import { addDepartment, getDepartments } from "../controllers/departmentController.js";
+import { addDepartment, getDepartments, editDepartment, updateDepartment, deleteDepartment } from "../controllers/departmentController.js";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
 
 
 const router = express.Router();
-router.get("/",getDepartments)
-router.post("/add",addDepartment)
+// router.get("/",isAuthenticated,getDepartments);
+router.get("/",getDepartments);
+router.post("/add",addDepartment);
+router.get("/:id",editDepartment);
+router.put("/:id",updateDepartment);
+router.delete("/:id",deleteDepartment);
 
 export default router
